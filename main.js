bfd9c2b78d68359e4197e2d437a38f6820a4a937
fritzgerald <13518065@std.stei.itb.ac.id>
2021-02-24 19:38:25 +0700

    save load

--- main.js
+++ main.js
@@ -99,6 +99,7 @@ function main() {
                     mode: gl.LINES
                 }
             )
+            console.log(objects)
         } else if (buttonState === 'square-button') {
             // add square
         } else if (buttonState === 'polygon-button') {
@@ -110,6 +111,55 @@ function main() {
             buttonState = 'none';
         }
     });
+
+    // save canvas to json
+    function canvasToJSON(objects){
+        out = [];
+
+        for (var i = 0; i < objects.length; i++){
+            var object = {
+                vertices: objects[i].vertices,
+                mode: objects[i].mode,
+                color: objects[i].color
+            }
+
+            out.push(object)
+        }
+
+        return out;
+    }
+
+    document.getElementById("save-button").addEventListener("click", function(){
+        data = canvasToJSON(objects)
+        jsonData = JSON.stringify(data)
+
+        var filename = prompt("filename:", "data")
+        if (filename == null){
+            filename = "data";
+        }
+
+        var file = new Blob([jsonData], {
+            type : "application/json"
+        })
+
+        var a = document.createElement("a")
+        a.href = URL.createObjectURL(file)
+        a.download = filename + ".json"
+        a.click()
+    });
+
+    // load json to canvas
+    var fileReader =  new FileReader()
+
+    document.getElementById("load-button").addEventListener("change", function(){
+        if (this.files[0]){
+            fileReader.readAsText(this.files[0]);
+        }
+    });
+    
+    fileReader.onload = function(){
+        objects = JSON.parse(fileReader.result);
+    };
 
     requestAnimationFrame(render);
 
