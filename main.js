5b345f9259a584e484394919647e4b23d02d5ae9
jonetmurti <composer202j@gmail.com>
2021-02-24 19:26:37 +0700

    implement square scaling

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function main() {
     var objects = [];
     var activeObject = null;
-    var id = 0;
     var startX = 0;
     var startY = 0;
     var isMove = false;
     var oldVertices = [];
+    var oldMidPoint = []; // only for square
     var chosenIdx = -1;
     var mousePos = {
         x: 0,
@@ -51,9 +51,13 @@ function main() {
     
     attribute vec2 vertPos;
     uniform vec2 screenRes;
+    uniform mat3 transToOrigin;
+    uniform mat3 scaleMat;
+    uniform mat3 transBack;
 
     void main() {
-        vec2 newPos = 2.0*vertPos/screenRes;
+        vec3 transResult = transBack * scaleMat * transToOrigin * vec3(vertPos, 1.0);
+        vec2 newPos = 2.0*transResult.xy/screenRes;
         gl_Position = vec4(newPos, 0.0, 1.0);
     }`;
 
@@ -96,17 +100,54 @@ function main() {
     canvas.addEventListener('mousedown', canvasOnMouseDown);
     canvas.addEventListener('mouseup', canvasOnMouseUp);
 
+    let slider = document.getElementById('square-slider');
+    slider.addEventListener('input', function() {
+        document.getElementById('value-tester').innerText = slider.value;
+        if (activeObject) {
+            if (activeObject.type === 'square') {
+                const newK = 0.1 * slider.value;
+                let translateToOrigin = [
+                    1.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0,
+                    -activeObject.midPoint[0], -activeObject.midPoint[1], 1.0
+                ];
+                let scaleMat = [
+                    newK, 0.0, 0.0,
+                    0.0, newK, 0.0, 
+                    0.0, 0.0, 1.0
+                ];
+                let translateBack = [
+                    1.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0,
+                    activeObject.midPoint[0], activeObject.midPoint[1], 1.0
+                ];
+                activeObject.k = newK;
+                activeObject.transToOrigin = translateToOrigin;
+                activeObject.scaleMat = scaleMat;
+                activeObject.transBack = translateBack;
+            }
+        }
+    });
+
     // Initialize uniforms
     gl.useProgram(program);
     let resLocation = gl.getUniformLocation(program, 'screenRes');
     gl.uniform2fv(resLocation, [gl.canvas.width, gl.canvas.height]);
 
+    console.log(
+        matrixTranspose([
+            1, 2, 3,
+            4, 5, 6,
+            7, 8, 9
+        ])
+    );
+
     requestAnimationFrame(render);
 
     function canvasOnMouseMove(event) {
-        var rect = canvas.getBoundingClientRect();
-        var xMax = gl.canvas.width / 2;
-        var yMax = gl.canvas.height / 2;
+        let rect = canvas.getBoundingClientRect();
+        let xMax = gl.canvas.width / 2;
+        let yMax = gl.canvas.height / 2;
 
         mousePos.x = event.clientX - rect.x - xMax;
         mousePos.y = rect.y - event.clientY + yMax;
@@ -127,13 +168,50 @@ function main() {
                         0, 1.0, 0,
                         0, 0, 1.0
                     ],
+                    transToOrigin: [
+                        1.0, 0, 0,
+                        0, 1.0, 0,
+                        0, 0, 1.0
+                    ],
+                    transBack: [
+                        1.0, 0, 0,
+                        0, 1.0, 0,
+                        0, 0, 1.0
+                    ],
                     mode: gl.LINES
                 }
             )
-            console.log(objects);
-            id++;
         } else if (buttonState === 'square-button') {
-            // add square
+            objects.push(
+                {
+                    type: 'square',
+                    midPoint: [mousePos.x + gl.canvas.width/16, mousePos.y - gl.canvas.height/16],
+                    vertices: [
+                        mousePos.x, mousePos.y,
+                        mousePos.x + gl.canvas.width/8, mousePos.y,
+                        mousePos.x + gl.canvas.width/8, mousePos.y - gl.canvas.width/8,
+                        mousePos.x, mousePos.y,
+                        mousePos.x + gl.canvas.width/8, mousePos.y - gl.canvas.width/8,
+                        mousePos.x, mousePos.y - gl.canvas.width/8
+                    ],
+                    k: 1.0,
+                    scaleMat: [
+                        1.0, 0, 0,
+                        0, 1.0, 0,
+                        0, 0, 1.0
+                    ],
+                    transToOrigin: [
+                        1.0, 0, 0,
+                        0, 1.0, 0,
+                        0, 0, 1.0
+                    ],
+                    transBack: [
+                        1.0, 0, 0,
+                        0, 1.0, 0,
+                        0, 0, 1.0
+                    ],
+                    mode: gl.TRIANGLES
+                });
         } else if (buttonState === 'polygon-button') {
             // add polygon
         }
@@ -147,10 +225,16 @@ function main() {
     function canvasOnMouseDown(event) {
         activeObject = null;
         isMove = false;
+        chosenIdx = -1;
         for (object of objects) {
             oldVertices = [];
+            oldMidPoint = [];
             for (let i = 0; i < object.vertices.length; i += 2) {
-                let transformedVertice = transform(object.scaleMat, [object.vertices[i], object.vertices[i + 1], 1.0])
+                let mat = matrixMultiplication(
+                    matrixTranspose(object.transBack), 
+                    matrixMultiplication(matrixTranspose(object.scaleMat), matrixTranspose(object.transToOrigin))
+                );
+                let transformedVertice = transform(mat, [object.vertices[i], object.vertices[i + 1], 1.0])
                 oldVertices.push(object.vertices[i]);
                 oldVertices.push(object.vertices[i + 1]);
                 if (Math.abs(transformedVertice[0] - mousePos.x) < 5 && Math.abs(transformedVertice[1] - mousePos.y) < 5) {
@@ -159,11 +243,53 @@ function main() {
                     startX = mousePos.x;
                     startY = mousePos.y;
                     chosenIdx = i;
+                    if (object.type === 'square') {
+                        oldMidPoint.push(object.midPoint[0]);
+                        oldMidPoint.push(object.midPoint[1]);
+                    }
                 }
             }
             if (isMove)
                 break;
         }
+    }
+
+    function matrixTranspose(mat) {
+        let copyMat = [];
+        for (elmt of mat) {
+            copyMat.push(elmt);
+        }
+
+        for (let i = 0; i < 3; i++) {
+            for (let j = i; j < 3; j++) {
+                let temp = copyMat[3*i + j];
+                copyMat[3*i + j] = copyMat[3*j + i];
+                copyMat[3*j + i] = temp;
+            }
+        }
+
+        return copyMat;
+    }
+
+    function matrixMultiplication(matA, matB) {
+
+        if (matA.length != matB.length) {
+            return;
+        }
+    
+        let outputMat = [];
+        let matLength = 3;
+        for (let i = 0; i < matLength; i++) {
+            for (let j = 0; j < matLength; j++) {
+                let sum = 0;
+                for (let k = 0; k < matLength; k++) {
+                    sum += matA[i*matLength + k] * matB[k*matLength + j];
+                }
+                outputMat.push(sum);
+            }
+        }
+    
+        return outputMat;
     }
 
     function transform(mat, vertice) {
@@ -181,18 +307,16 @@ function main() {
     }
 
     function canvasOnMouseUp(event) {
-        document.getElementById('mouse-button').innerText = 'lepas';
         isMove = false;
-        chosenIdx = -1;
     }
 
     function drawObject(object, program) {
-        var buf = gl.createBuffer();
+        let buf = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, buf);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(object.vertices), gl.STATIC_DRAW);
 
         gl.useProgram(program);
-        var pos = gl.getAttribLocation(program, 'vertPos');
+        let pos = gl.getAttribLocation(program, 'vertPos');
         gl.vertexAttribPointer(
             pos,
             2,
@@ -202,12 +326,19 @@ function main() {
             0
         );
         gl.enableVertexAttribArray(pos);
+
+        let matLocation = gl.getUniformLocation(program, 'transToOrigin');
+        gl.uniformMatrix3fv(matLocation, false, new Float32Array(object.transToOrigin));
+        matLocation = gl.getUniformLocation(program, 'scaleMat');
+        gl.uniformMatrix3fv(matLocation, false, new Float32Array(object.scaleMat));
+        matLocation = gl.getUniformLocation(program, 'transBack');
+        gl.uniformMatrix3fv(matLocation, false, new Float32Array(object.transBack));
+
         gl.drawArrays(object.mode, 0, object.vertices.length/2);
     }
 
     function moveObject() {
         if (isMove) {
-            console.log('moving');
             let xTrans = mousePos.x - startX;
             let yTrans = mousePos.y - startY;
             if (activeObject.type === 'line') {
@@ -217,6 +348,23 @@ function main() {
                 for (let i = 0; i < activeObject.vertices.length; i += 2) {
                     activeObject.vertices[i] = oldVertices[i] + xTrans;
                     activeObject.vertices[i + 1] = oldVertices[i + 1] + yTrans;
+                }
+
+                if (activeObject.type === 'square') {
+                    activeObject.midPoint[0] = oldMidPoint[0] + xTrans;
+                    activeObject.midPoint[1] = oldMidPoint[1] + yTrans;
+                    let translateToOrigin = [
+                        1.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0,
+                        -activeObject.midPoint[0], -activeObject.midPoint[1], 1.0
+                    ];
+                    let translateBack = [
+                        1.0, 0.0, 0.0,
+                        0.0, 1.0, 0.0,
+                        activeObject.midPoint[0], activeObject.midPoint[1], 1.0
+                    ];
+                    activeObject.transToOrigin = translateToOrigin;
+                    activeObject.transBack = translateBack;
                 }
             }
         }
